ce044e0408a9555d1ec91a3d92b3e46330f3182e
Aivuo <rioneda1@gmail.com>
2018-04-05 15:03:14 +0200

    Added Subtraction, Multiplication and Division

--- Kalkylator/Kalkylator/Program.cs
+++ Kalkylator/Kalkylator/Program.cs
@@ -25,13 +25,14 @@ namespace Kalkylator
             int intChoice = 0;
 
             Console.Clear();
-            Console.WriteLine("Welcome to the Calculator");
-            Console.WriteLine("Choose your function: ");
-            Console.WriteLine("1. Addition");
-            Console.WriteLine("2. Subtraction");
-            Console.WriteLine("3. Multiplication");
-            Console.WriteLine("4. Division");
-            Console.WriteLine("5. End");
+            Console.WriteLine("Welcome to the Calculator" + "\n"
+                            + "Choose your function:" + "\n"
+                            + "1. Addition" + "\n"
+                            + "2. Subtraction" + "\n"
+                            + "3. Multiplication" + "\n"
+                            + "4. Division" + "\n"
+                            + "5. End" + "\n"
+                );
             choice = Console.ReadLine();
 
             int.TryParse(choice, out intChoice);
@@ -46,19 +47,19 @@ namespace Kalkylator
                     Subtraction();
                     break;
                 case 3:
-
+                    Multiplication();
                     break;
 
                 case 4:
-
+                    Division();
                     break;
 
                 case 5:
                     return false;
-                    break;
 
                 default:
-                    Console.WriteLine(choice + " is not a valid option!");
+                    Console.WriteLine("\n" + choice + " is not a valid option!");
+                    Console.ReadKey();
                     break;
             }
 
@@ -74,6 +75,7 @@ namespace Kalkylator
             float secondParameter = 0;
 
             Console.Clear();
+            Console.WriteLine("Welcome to Addition!");
             Console.WriteLine("Give me the first number: ");
 
             parameterString = Console.ReadLine();
@@ -96,6 +98,95 @@ namespace Kalkylator
         static void Subtraction()
         {
             float answer = 0;
+
+            String parameterString;
+            float firstParameter = 0;
+            float secondParameter = 0;
+
+            Console.Clear();
+            Console.WriteLine("Welcome to Subtraction!");
+            Console.WriteLine("Give me the first number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out firstParameter);
+
+            Console.WriteLine("Now give me a second number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out secondParameter);
+
+            answer = firstParameter - secondParameter;
+            Console.WriteLine("The answer is: " + answer);
+            Console.WriteLine("Return to menu y/n");
+            if (Console.ReadLine() == "y")
+                StartMenu();
+            else
+                Subtraction();
+        }
+
+        static void Multiplication()
+        {
+            float answer = 0;
+
+            String parameterString;
+            float firstParameter = 0;
+            float secondParameter = 0;
+
+            Console.Clear();
+            Console.WriteLine("Welcome to Multiplication!");
+            Console.WriteLine("Give me the first number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out firstParameter);
+
+            Console.WriteLine("Now give me a second number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out secondParameter);
+
+            answer = firstParameter * secondParameter;
+            Console.WriteLine("The answer is: " + answer);
+            Console.WriteLine("Return to menu y/n");
+            if (Console.ReadLine() == "y")
+                StartMenu();
+            else
+                Multiplication();
+        }
+
+        static void Division()
+        {
+            float answer = 0;
+
+            String parameterString;
+            float firstParameter = 0;
+            float secondParameter = 0;
+
+            Console.Clear();
+            Console.WriteLine("Welcome to Division!");
+            Console.WriteLine("Give me the first number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out firstParameter);
+
+            Console.WriteLine("Now give me a second number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out secondParameter);
+
+            if (secondParameter == 0)
+            {
+                Console.WriteLine("0 is not an acceptable second parameter!");
+                Console.ReadKey();
+                Division();
+            }
+
+            answer = firstParameter / secondParameter;
+            Console.WriteLine("The answer is: " + answer);
+            Console.WriteLine("Return to menu y/n");
+            if (Console.ReadLine() == "y")
+                StartMenu();
+            else
+                Division();
         }
     }
 }
