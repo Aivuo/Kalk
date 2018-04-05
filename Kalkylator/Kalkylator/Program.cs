db49c856155d7735f7fec7ae2efcad9bbafeba2f
Aivuo <rioneda1@gmail.com>
2018-04-05 14:43:58 +0200

    Added Addition functionality

--- Kalkylator/Kalkylator/Program.cs
+++ Kalkylator/Kalkylator/Program.cs
@@ -10,13 +10,92 @@ namespace Kalkylator
     {
         static void Main(string[] args)
         {
+            bool runningProgram = true;
+
+
+            while (runningProgram == true)
+            {
+                runningProgram = StartMenu();
+            }
         }
 
-        float Addition(float number1, float number2)
+        static bool StartMenu()
+        {
+            string choice;
+            int intChoice = 0;
+
+            Console.Clear();
+            Console.WriteLine("Welcome to the Calculator");
+            Console.WriteLine("Choose your function: ");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. End");
+            choice = Console.ReadLine();
+
+            int.TryParse(choice, out intChoice);
+
+            switch (intChoice)
+            {
+                case 1:
+                    Addition();
+                    break;
+
+                case 2:
+                    Subtraction();
+                    break;
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+                    return false;
+                    break;
+
+                default:
+                    Console.WriteLine(choice + " is not a valid option!");
+                    break;
+            }
+
+            return true;
+        }
+
+        static void Addition()
         {
             float answer = 0;
 
-            return(answer);
+            String parameterString;
+            float firstParameter = 0;
+            float secondParameter = 0;
+
+            Console.Clear();
+            Console.WriteLine("Give me the first number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out firstParameter);
+
+            Console.WriteLine("Now give me a second number: ");
+
+            parameterString = Console.ReadLine();
+            float.TryParse(parameterString, out secondParameter);
+
+            answer = firstParameter + secondParameter;
+            Console.WriteLine("The answer is: " + answer);
+            Console.WriteLine("Return to menu y/n");
+            if (Console.ReadLine() == "y")
+                StartMenu();
+            else
+                Addition();
+        }
+
+        static void Subtraction()
+        {
+            float answer = 0;
         }
     }
 }
